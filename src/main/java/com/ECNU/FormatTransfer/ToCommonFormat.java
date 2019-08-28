d0579cef7aed8f8216877754d52ba4f2cbf00e5a
bianhan258369 <1399634971@qq.com>
2019-08-28 20:14:41 +0800

    add NuSMV

---
+++ src/main/java/com/ECNU/FormatTransfer/ToCommonFormat.java
@@ -0,0 +1,134 @@
+package com.ECNU.FormatTransfer;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class ToCommonFormat {
+    protected int count;
+    protected String path;
+    protected String fileNameSrc;
+    protected String fileNameDest;
+    // get System newLine
+    protected static String lineSeparator = System.getProperty("line.separator");
+    // get System fileSeparator
+    protected static String fileSeparator = System.getProperty("file.separator");
+    // process the ToNuSMVFormat/ToMyCCLFormat Exception
+    protected static StringBuilder exception = new StringBuilder();
+    protected static final String[] str = {"Union","Boundeddiff","StrictPre","Coincidence","Alter",
+            "Exclusion","Cause" };
+    // All clock keyWordsString to ArrayList<String>
+    protected static final List<String> clockKws = Arrays.asList(str);
+    // Store All Clock Phenomena
+    protected List<String> clocks;
+    // Store All Clock Constraints
+    protected List<String> clockConstrans;
+
+    protected ToCommonFormat(String fileNameSrc, String fileNameDest, String path) {
+        this.clocks = new ArrayList<>();
+        this.clockConstrans = new ArrayList<>();
+        this.path = path;
+        this.count = 1;
+        this.fileNameSrc = fileNameSrc;
+        this.fileNameDest = fileNameDest;
+        initClock();
+    }
+
+    protected void initClock() {
+        // find the last semicolon of all phenomena
+        boolean isLastSemicoPhe = false;
+        try(BufferedReader br = new BufferedReader(new FileReader(path + fileSeparator +fileNameSrc));) {
+            String read;
+            while((read = br.readLine()) != null) {
+                int index = read.lastIndexOf(';');
+                // store all clock phenomena,except the clock with Semicolon
+                if (index == -1 && !isLastSemicoPhe) {
+                    clocks.add(read);
+                } // store the clock with Semicolon
+                else if (index != -1 && !isLastSemicoPhe) {
+                    String readSub = read.substring(0,read.length()-1);
+                    clocks.add(readSub);
+                    isLastSemicoPhe = true;
+                } // store all clock constraints,including the last Semicolon
+                else if (index != -1 && isLastSemicoPhe) {
+                    // remove the last Semicolon of a line
+                    read = read.substring(0,read.length()-1);
+                    clockConstrans.add(read);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("找不到文件...");
+        } catch (IOException e) {
+            System.out.println("读取文件失败...");
+        }
+    }
+
+    protected abstract void writeAll();
+
+    protected abstract void beginWrite(Writer fw) throws IOException;
+
+    protected void writeTopSection(File fileDest) {
+        Writer fileWriter = null;
+        try {
+            if (!fileDest.exists()) {
+                fileDest.createNewFile();
+            }
+            fileWriter = new BufferedWriter(new FileWriter(fileDest));
+            beginWrite(fileWriter);
+            judgeAndWrite(fileWriter);
+        } catch (Exception e) {
+            //e.printStackTrace();
+            exception.append("There are problems in " + fileNameSrc + " !!!");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fileWriter)
+                    fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected abstract void judgeAndWrite(Writer fw) throws Exception;
+
+    protected String judgeKw(String[] temp) {
+        for(String s:clockKws) {
+            for(String str:temp) {
+                if (str.equals(s)) {
+                    return s;
+                }
+            }
+        }
+        return "";
+    }
+
+    protected void writeBottomSection(File fileAppend, File fileDest) {
+        // read by line and store in a List
+        List<String> msgs = null;
+        try {
+            msgs = FileUtils.readLines(fileAppend,"GBK");
+            for(Iterator<String> iter = msgs.iterator(); iter.hasNext();) {
+                String msg = iter.next();
+                // the last argument represent "append" mode is "true"
+                FileUtils.writeStringToFile(fileDest,
+                        msg + lineSeparator,"GBK",true);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getException() {
+        return exception.toString();
+    }
+
+    public static void clearException() {
+        // clear StringBuilder object "exception"
+        exception.delete(0,exception.length());
+    }
+}
---
+++ src/main/java/com/ECNU/FormatTransfer/ToNuSMVFormat.java
@@ -0,0 +1,126 @@
+package com.ECNU.FormatTransfer;
+
+import java.io.*;
+import java.util.*;
+
+public class ToNuSMVFormat extends ToCommonFormat {
+    // give StrictPre and Cause Bound
+    private int bound = 5;
+    // consistent formular
+    private String consistentFormular;
+
+    public ToNuSMVFormat(String fileNameSrc, String fileNameDest, String path, int bound) {
+        super(fileNameSrc,fileNameDest,path);
+        this.bound = bound;
+        writeAll();
+    }
+
+    @Override
+    public void writeAll() {
+        File fileDest = new File(path + fileSeparator + this.fileNameDest + ".smv");
+        writeTopSection(fileDest);
+        // append File "next.txt" content
+        File fileAppend = new File("NuSMV/DONOTMOVE/next.txt");
+        // the file needed to be appended
+        // File fileDest = new File(path + fileSeparator + this.fileNameDest + ".smv");
+        writeBottomSection(fileAppend,fileDest);
+    }
+
+    @Override
+    public void beginWrite(Writer fw) throws IOException {
+        fw.write("MODULE main" + lineSeparator);
+        fw.write(lineSeparator);
+        fw.write("VAR" + lineSeparator);
+        for(String clock:clocks) {
+            fw.write(clock + ":boolean;" + lineSeparator);
+        }
+        fw.write(lineSeparator);
+    }
+
+    @Override
+    public void judgeAndWrite(Writer fw) throws Exception {
+        String[] splitList = null;
+        String kw = "";
+        for(Iterator<String> iter = clockConstrans.iterator(); iter.hasNext();) {
+            String temp = iter.next();
+            // To escape(转义)
+            // "\s" you need two backslashes(反斜线) since the backslash is a special character in Java strings,
+            // and needs to be escaped itself.
+            // So, it becomes: "\\s"
+            splitList = temp.split("\\s+");
+            kw = judgeKw(splitList);
+            fw.write("ctr" + count);
+            if (kw.equals("Union")) {
+                fw.write(":unionn(");
+                fw.write(splitList[0] + "," + splitList[2] + "," +
+                        splitList[3].substring(1,splitList[3].length()-1));
+                fw.write(");" + lineSeparator);
+                count++;
+            }else if (kw.equals("Boundeddiff")) {
+                String tmpStr = splitList[2].substring(1,splitList[2].length()-1);
+                String downBound = "0";
+                String upBound = "";
+                if (tmpStr.indexOf(',') != -1) {
+                    String[] tmpStrList = tmpStr.split(",");
+                    downBound = tmpStrList[0];
+                    upBound = tmpStrList[1];
+                }else {
+                    upBound = tmpStr;
+                }
+                fw.write(":boundeddiff(");
+                fw.write(splitList[0] + "," + splitList[3] + "," + downBound + "," + upBound);
+                fw.write(");" + lineSeparator);
+                count++;
+            }else if (kw.equals("StrictPre")) {
+                fw.write(":strictpre(");
+                fw.write(splitList[0] + "," + splitList[2] + ",");
+                fw.write(bound + ");" + lineSeparator);
+                count++;
+            }else if (kw.equals("Coincidence")) {
+                fw.write(":coincidence(");
+                fw.write(splitList[0] + "," + splitList[2] + ");" + lineSeparator);
+                count++;
+            }else if(kw.equals("Alter")) {
+                fw.write(":Alter(");
+                fw.write(splitList[0] + "," + splitList[2] + ");" + lineSeparator);
+                count++;
+            }else if(kw.equals("Exclusion")) {
+                fw.write(":exclusion(");
+                fw.write(splitList[0] + "," + splitList[2] + ");" + lineSeparator);
+                count++;
+            }else if(kw.equals("Cause")) {
+                fw.write(":cause(");
+                fw.write(splitList[0] + "," + splitList[2] + ",");
+                fw.write(bound + ");" + lineSeparator);
+                count++;
+            }
+        }
+
+        // init All Clock Phenomena "false"
+        fw.write(lineSeparator + "ASSIGN" + lineSeparator);
+        for(Iterator<String> iter=clocks.iterator();iter.hasNext();) {
+            String temp = iter.next();
+            fw.write("init(" + temp + ") := FALSE;" + lineSeparator);
+        }
+
+        // write consistent formula
+        StringBuilder stringBuilder = new StringBuilder("");
+        stringBuilder.append("CTLSPEC ( ! EF AG !( ");
+        for(String clock:clocks) {
+            stringBuilder.append(clock + "|");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        stringBuilder.append(" ) )");
+        consistentFormular = stringBuilder.toString();
+        fw.write(consistentFormular + lineSeparator + lineSeparator);
+    }
+
+    public String getConsistentFormular() {
+        return consistentFormular;
+    }
+
+    public static void main(String[] args) {
+        new ToNuSMVFormat("ISCCSL.txt","ISCCSL"
+                ,"NuSMV",5);
+    }
+}
